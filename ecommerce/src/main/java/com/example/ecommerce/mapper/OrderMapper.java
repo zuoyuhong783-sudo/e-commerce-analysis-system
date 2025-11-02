775af25bdcfa3f9a2fa1033223e47eafd97005f0
zuoyuhong783-sudo <zuoyuhong783@gmail.com>
2025-11-02 22:37:49 +0800

    Update OrderMapper.java

--- ecommerce/src/main/java/com/example/ecommerce/mapper/OrderMapper.java
+++ ecommerce/src/main/java/com/example/ecommerce/mapper/OrderMapper.java
@@ -9,25 +9,12 @@ import java.util.Map;
 @Mapper
 public interface OrderMapper {
 
-    /**
-     * 根据动态条件统计订单数量
-     * @param map
-     * @return
-     */
+
     Integer countByMap(Map<String, Object> map);
 
-    /**
-     * 根据动态条件统计订单金额
-     * @param map
-     * @return
-     */
+
     Double sumByMap(Map<String, Object> map);
     
-    /**
-     * 统计指定时间区间内的销量排名前十
-     * @param begin
-     * @param end
-     * @return
-     */
+
     List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
