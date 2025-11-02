54148972f6ff580b18fc63bc22927db278a24b2e
zuoyuhong783-sudo <zuoyuhong783@gmail.com>
2025-11-02 22:32:48 +0800

    Update RedisConfig.java

--- ecommerce/src/main/java/com/example/ecommerce/config/RedisConfig.java
+++ ecommerce/src/main/java/com/example/ecommerce/config/RedisConfig.java
@@ -12,23 +12,23 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        // 创建 RedisTemplate 对象
+        
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        // 设置连接工厂
+        
         template.setConnectionFactory(connectionFactory);
 
-        // 创建 JSON 序列化工具
+        
         GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
 
-        // 设置 Key 的序列化方式为 String
+        
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
 
-        // 设置 Value 的序列化方式为 JSON
+        
         template.setValueSerializer(jsonRedisSerializer);
         template.setHashValueSerializer(jsonRedisSerializer);
 
-        // 初始化 RedisTemplate
+        
         template.afterPropertiesSet();
         
         return template;
